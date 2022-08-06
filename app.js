fc9654abd62bb2f29cfbfa5eb58222aa8a5ebcdb
Ankit Gupta <ag3831124@gmail.com>
2022-08-06 23:32:13 +0530

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 const bodyparser = require("body-parser");
 // Body-parser middleware
 app.use(bodyparser.urlencoded({ extended: false }));
